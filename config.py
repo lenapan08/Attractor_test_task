e12853f47023415c7ff4022e4eae5a07f4115149
lenapan08 <83510476+lenapan08@users.noreply.github.com>
2022-02-13 14:53:34 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+#тестовые данные
+
+class TestData:
+    BASE_URL = "https://the-internet.herokuapp.com/"
+    USERNAME = "tomsmith"
+    PASSWORD = "SuperSecretPassword!"
+    SECURE_AREA = "Secure Area"
+    KEY_PRESSES_INPUT_DATA = "l"
+    RESULT_INPUT_DATA ="You entered: L"
+    HREF_FILE = 'download/details.txt'
+    PART_IN_FILE = 'adcb'
+
